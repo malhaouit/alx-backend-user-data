793b2ed2e5ad6e44af057614c076e79a4ef08b48
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-06 20:18:12 +0100

    Implements the class `BasicAuth`

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,19 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ This class for now is empty
+    """ Implements a Basic Authentication
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extracts the Base64 part from the Authorization header
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header[6:]
