7301936cb67fa6e1a8ed9dfaa782edcf29aa6158
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-07 09:59:09 +0100

    Add `Base64` decoding to `BasicAuth` with error handling

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Covers a Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decodes the value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(
+                    base64_authorization_header, validate=True)
+            return decoded_bytes.decode('utf-8')
+        except Exception as e:
+            return None
