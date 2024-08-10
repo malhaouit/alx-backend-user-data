0e4cf96ef1297d259e5616d5b1b824dad8527c91
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-10 10:34:45 +0100

    Update: handle the new environment variable `AUTH_TYPE=session_auth`

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ auth_type = os.getenv('AUTH_TYPE')
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
