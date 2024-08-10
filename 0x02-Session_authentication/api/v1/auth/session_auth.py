103fa34a1cc7e23a1ac2c1fb6735379a9a4865c1
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-10 10:33:08 +0100

    Add `session_auth.py` file that represents an empty session class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module implement session authentication.
+"""
+
+
+class SessionAuth(Auth):
+    """ This class inherits from Auth class and implement
+    session authentication.
+    """
+    pass
