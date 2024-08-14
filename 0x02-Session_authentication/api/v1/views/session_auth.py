6a10777eccbc5e17232070079326f38caae4ce6b
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-14 11:50:49 +0100

    Update: add if condition to check if users list is empty

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -26,9 +26,12 @@ def login():
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
-    for user in users:
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
 
