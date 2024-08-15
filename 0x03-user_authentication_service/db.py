0ebae95512d998a7bc7838f672a309296f84f59a
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-15 12:40:45 +0100

    Update: import TypeVar

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import User
+from typing import TypeVar
 
 from user import Base
 
