57bfed23c0647d77a59687391afcd373f35613e3
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-15 22:17:55 +0100

    Implement user registration with hashed password and duplicate email check

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
-"""Auth module for handling password hashing
+"""Auth module for handling user authentication.
 """
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
+from typing import Optional
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt and returns the salted hash as bytes
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user if they don't already exist.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(
+                    email, hashed_password.decode('utf-8'))
+        return new_user
