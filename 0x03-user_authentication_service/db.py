fd9a2dddd6cf4d4392549c36b1aeff545f05c67d
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-15 12:54:56 +0100

    Update: change return type annotation of `add_user` method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from typing import TypeVar
 
 from user import Base, User
 
@@ -30,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
