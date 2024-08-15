a89b6086c835b6229b4e555d6681f3db32586411
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-15 22:27:02 +0000

    Update: Implement login route to handle user session creation and authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,5 +27,27 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Handles user login and creates a session."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or password is None:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
