9cd798c61cff7c07c8eda4cf2165d1a7c2ee1017
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-18 14:20:00 +0100

    Update destroy_session method by removing one line of code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,7 +74,6 @@ class Auth:
             return None
 
         try:
-            user = self._db.find_user_by(id=user_id)
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
