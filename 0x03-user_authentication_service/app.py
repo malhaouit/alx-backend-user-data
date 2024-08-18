03a139c99557648f7251f08d2c7521b86f3320e4
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-18 13:52:57 +0100

    Update app.py module: import abort and make_response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app for user registration"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
