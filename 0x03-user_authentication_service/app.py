c708ac39b348c5bf5a70bc8249fddd4c95f8cde7
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-18 21:09:15 +0100

    Add a new route to handle token generation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,5 +89,21 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """Get a reset password token to update the password
+    """
+    email = request.form.get('email')
+
+    if email is None:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
